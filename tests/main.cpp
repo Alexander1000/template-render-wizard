07f6c470a5c4a811c8eddb7e6b9c86064559030a
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-07-26 08:30:12 +0300

    add test suite

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,21 @@
 #include <cpp-unit-test.h>
 
+CppUnitTest::TestCase* testParseToken_Template_Positive() {
+    CppUnitTest::TestCase* t = nullptr;
+    t = new CppUnitTest::TestCase("001-simple-object");
+
+    t->printTitle();
+
+    t->finish();
+    return t;
+}
+
 int main(int argc, char** argv) {
+    CppUnitTest::TestSuite testSuite;
+
+    testSuite.addTestCase(testParseToken_Template_Positive());
+
+    testSuite.printTotal();
+
     return 0;
 }
