8f73c925215fc04fec44a938fe264cdfc3a89167
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-07-26 12:51:13 +0300

    add test assert

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,22 @@
+#include <trw.h>
+#include <io-buffer.h>
 #include <cpp-unit-test.h>
 
 CppUnitTest::TestCase* testParseToken_Template_Positive() {
     CppUnitTest::TestCase* t = nullptr;
-    t = new CppUnitTest::TestCase("001-simple-object");
+    t = new CppUnitTest::TestCase("001-simple-text");
 
     t->printTitle();
+
+    TemplateRenderWizard::Token::Token* token = nullptr;
+
+    IOBuffer::IOFileReader fileReader("./fixtures/001-simple-text.txt");
+    IOBuffer::CharStream charStream(&fileReader);
+    TemplateRenderWizard::Stream tokenStream(&charStream);
+
+    token = tokenStream.getNextToken();
+
+    CppUnitTest::assertNotNull(t, token);
 
     t->finish();
     return t;
