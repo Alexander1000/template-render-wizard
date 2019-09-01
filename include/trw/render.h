fdefc18075aede21b7b28c46cd22e6431a2b8bc8
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-09-01 11:34:47 +0300

    render class

--- include/trw/render.h
+++ include/trw/render.h
@@ -1,3 +1,23 @@
 #ifndef H_TRW_RENDER_INCLUDED
 #define H_TRW_RENDER_INCLUDED
+
+#include <string>
+#include <trw/stream.h>
+#include <trw/tree_parameters.h>
+
+namespace TemplateRenderWizard
+{
+    class Render
+    {
+        TemplateRenderWizard::Tree::Tree* tree;
+        TemplateRenderWizard::Stream* stream;
+
+    public:
+        Render(std::string*, TemplateRenderWizard::Tree::Tree*);
+        Render(TemplateRenderWizard::Stream*, TemplateRenderWizard::Tree::Tree*);
+        void save();
+        void save(std::string*);
+    };
+}
+
 #endif
