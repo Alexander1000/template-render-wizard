d8e4b292df18c1b8191caed689e4fd3879bbf0c4
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-09-15 12:47:58 +0300

    draft render class

--- src/trw/render.cpp
+++ src/trw/render.cpp
@@ -1,4 +1,24 @@
 #include <trw/render.h>
+#include <io-buffer.h>
 
 namespace TemplateRenderWizard
-{}
+{
+    Render::Render(std::string* srcTemplateFile, TemplateRenderWizard::Tree::Tree* tree)
+    {
+        IOBuffer::IOFileReader fileReader(srcTemplateFile->c_str());
+        IOBuffer::CharStream charStream(&fileReader);
+        this->stream = new TemplateRenderWizard::Stream(&charStream);
+        this->tree = tree;
+    }
+
+    Render::Render(TemplateRenderWizard::Stream* stream, TemplateRenderWizard::Tree::Tree* tree)
+    {
+        this->stream = stream;
+        this->tree = tree;
+    }
+
+    void Render::save()
+    {
+        // do render
+    }
+}
