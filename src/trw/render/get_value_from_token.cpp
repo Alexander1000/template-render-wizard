25d43391607b5e2e8b6c3eb97815bd6cc20e7e55
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-12-09 03:23:25 +0300

    optimize

--- src/trw/render/get_value_from_token.cpp
+++ src/trw/render/get_value_from_token.cpp
@@ -2,7 +2,6 @@
 
 namespace TemplateRenderWizard
 {
-
     Value* Render::getValueFromToken(Token::Token *token)
     {
         if (token->getType() != TemplateRenderWizard::Token::Type::ExpressionValueType) {
@@ -14,35 +13,25 @@ namespace TemplateRenderWizard
 
         auto leafValue = this->tree->get(tokenValue);
         if (leafValue != nullptr) {
-            switch(leafValue->getType()) {
-                case TemplateRenderWizard::Tree::LeafElementType::LeafElementText: {
-                    auto str = (std::string*) leafValue->getData();
-                    Value* v;
-                    v = new Value();
-                    int nSize = str->length() + 1;
-                    INIT_CHAR_STRING(newStr, nSize)
-                    memcpy(newStr, str->c_str(), (nSize - 1) * sizeof(char));
-                    v->setData(newStr);
-                    return v;
-                }
-                case TemplateRenderWizard::Tree::LeafElementType::LeafElementArray: {
-                    throw new UnexpectedToken;
-                    break;
-                }
-                case TemplateRenderWizard::Tree::LeafElementType::LeafElementObject: {
-                    throw new UnexpectedToken;
-                    break;
-                }
+            if (leafValue->getType() != TemplateRenderWizard::Tree::LeafElementType::LeafElementText) {
+                throw new UnexpectedToken;
             }
-        } else {
-            Value* v = new Value;
-            int length = strlen(tokenValue) + 1;
-            INIT_CHAR_STRING(strValue, length)
-            memcpy(strValue, tokenValue, sizeof(char) * length);
-            v->setData(strValue);
+
+            auto str = (std::string*) leafValue->getData();
+            Value* v;
+            v = new Value();
+            int nSize = str->length() + 1;
+            INIT_CHAR_STRING(newStr, nSize)
+            memcpy(newStr, str->c_str(), (nSize - 1) * sizeof(char));
+            v->setData(newStr);
             return v;
         }
 
-        return nullptr;
+        Value* v = new Value;
+        int length = strlen(tokenValue) + 1;
+        INIT_CHAR_STRING(strValue, length)
+        memcpy(strValue, tokenValue, sizeof(char) * length);
+        v->setData(strValue);
+        return v;
     }
 }
