9008e983ae40993aa71bdd49d703b5d4ece60664
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-12-12 14:14:34 +0300

    add fixtures out

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,6 +2,7 @@
 #include <io-buffer.h>
 #include <cpp-unit-test.h>
 #include <memory.h>
+#include <dirent.h>
 
 void assertEquals(CppUnitTest::TestCase* t, TemplateRenderWizard::Token::Type expectedTokenType, TemplateRenderWizard::Token::Type actualTokenType) {
     t->increment();
@@ -275,6 +276,111 @@ CppUnitTest::TestCase* testRender_TemplateWithNestedConditionsCase03_Positive()
     return t;
 }
 
+CppUnitTest::TestCase* testRender_Template_Positive(char* templateName, char* valuesFile)
+{
+    CppUnitTest::TestCase* t = nullptr;
+    t = new CppUnitTest::TestCase(valuesFile);
+
+    t->printTitle();
+
+    TemplateRenderWizard::Tree::Tree tree;
+    INIT_CHAR_STRING(fileWithValues, 1024)
+    sprintf(fileWithValues, "./fixtures/%s", valuesFile);
+    tree.scan(fileWithValues);
+
+    TemplateRenderWizard::Render* render;
+    INIT_CHAR_STRING(fileWithTemplate, 1024)
+    sprintf(fileWithTemplate, "./fixtures/%s", templateName);
+    render = new TemplateRenderWizard::Render(fileWithTemplate, &tree);
+
+    IOBuffer::IOMemoryBuffer* buffer = render->toBuffer();
+
+    INIT_CHAR_STRING(tBuffer, 1024);
+    buffer->read(tBuffer, 1024);
+
+    INIT_CHAR_STRING(expected, 1024)
+    INIT_CHAR_STRING(fileExpected, 1024)
+    memcpy(fileExpected, valuesFile, sizeof(char) * (strlen(valuesFile) - 5));
+
+    INIT_CHAR_STRING(strFileExpected, 1024)
+    sprintf(strFileExpected, "./fixtures/%s.out", fileExpected);
+
+    std::cout << "OutFile: " << strFileExpected << std::endl;
+
+    IOBuffer::IOFileReader* fileReader;
+    fileReader = new IOBuffer::IOFileReader(strFileExpected);
+    fileReader->read(expected, 1024);
+
+    std::cout << "Expected: " << expected << std::endl;
+
+    CppUnitTest::assertEquals(t, expected, tBuffer);
+
+    t->finish();
+    return t;
+}
+
+static int filter_tpl(const struct dirent* dir_ent)
+{
+    if (!strcmp(dir_ent->d_name, ".") || !strcmp(dir_ent->d_name, "..")) {
+        return 0;
+    }
+
+    std::string fname = dir_ent->d_name;
+
+    if (fname.find(".tpl") == std::string::npos) {
+        return 0;
+    }
+
+    return 1;
+}
+
+static int filter_yaml(const struct dirent* dir_ent)
+{
+    if (!strcmp(dir_ent->d_name, ".") || !strcmp(dir_ent->d_name, "..")) {
+        return 0;
+    }
+
+    std::string fname = dir_ent->d_name;
+
+    if (fname.find(".yaml") == std::string::npos) {
+        return 0;
+    }
+
+    return 1;
+}
+
+void scanTests(CppUnitTest::TestSuite* testSuite) {
+    // std::vector<std::string> v;
+
+    struct dirent **namelist;
+    struct dirent **yamlList;
+
+    int n = scandir("./fixtures", &namelist, *filter_tpl, alphasort);
+    int nValues = scandir("./fixtures", &yamlList, *filter_yaml, alphasort);
+
+    INIT_CHAR_STRING(strTplNum, 5);
+    INIT_CHAR_STRING(strYamlNum, 5);
+
+    for (int i = 0; i<n; i++) {
+        memset(strTplNum, 0, sizeof(char) * 5);
+        memcpy(strTplNum, namelist[i]->d_name, 3 * sizeof(char));
+
+        for (int j = 0; j < nValues; j++) {
+            memset(strYamlNum, 0, sizeof(char) * 5);
+            memcpy(strYamlNum, yamlList[j]->d_name, 3 * sizeof(char));
+
+            if (strcmp(strTplNum, strYamlNum) == 0) {
+                std::cout << "Link: " << namelist[i]->d_name << "; " << yamlList[j]->d_name << std::endl;
+                testSuite->addTestCase(testRender_Template_Positive(namelist[i]->d_name, yamlList[j]->d_name));
+            }
+        }
+
+        free(namelist[i]);
+        std::cout << std::endl;
+    }
+    free(namelist);
+}
+
 int main(int argc, char** argv) {
     CppUnitTest::TestSuite testSuite;
 
@@ -289,6 +395,8 @@ int main(int argc, char** argv) {
     testSuite.addTestCase(testRender_TemplateWithNestedConditionsCase01_Positive());
     testSuite.addTestCase(testRender_TemplateWithNestedConditionsCase02_Positive());
     testSuite.addTestCase(testRender_TemplateWithNestedConditionsCase03_Positive());
+
+    scanTests(&testSuite);
 
     testSuite.printTotal();
 
