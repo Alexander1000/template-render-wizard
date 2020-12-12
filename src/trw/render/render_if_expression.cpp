ee45da02913825afd9ad9236b6f5be71c77d39cb
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-12-12 12:07:36 +0300

    nested conditions

--- src/trw/render/render_if_expression.cpp
+++ src/trw/render/render_if_expression.cpp
@@ -12,6 +12,10 @@ namespace TemplateRenderWizard
 
         INIT_CHAR_STRING(keyword, 32)
 
+        int levelNested = 0;
+
+        std::cout << "Result: " << result << std::endl;
+
         while (token != nullptr) {
             switch (token->getType()) {
                 case TemplateRenderWizard::Token::Type::PlainTextType: {
@@ -45,24 +49,52 @@ namespace TemplateRenderWizard
                     token->getReader()->read(keyword, 32);
 
                     if (strcmp(keyword, "if") == 0) {
-                        this->pushBackToken(token);
-                        this->renderControlExpression(buffer);
+                        if (!skipBlock) {
+                            this->pushBackToken(token);
+                            this->renderControlExpression(buffer);
+                        } else {
+                            std::cout << "Level Nested Up" << std::endl;
+                            levelNested++;
+                            while (token->getType() != TemplateRenderWizard::Token::Type::CloseControlTagType) {
+                                // tokens.push_back(token);
+                                INIT_CHAR_STRING(sToken, 1024)
+                                RESET_TOKEN_READER(token)
+                                token->getReader()->read(sToken, 1024);
+                                std::cout << "Token: " << sToken << std::endl;
+                                token = this->getNextToken();
+                            }
+                            break;
+                        }
                     }
 
                     if (strcmp(keyword, "else") == 0) {
-                        skipBlock = result;
+                        if (levelNested == 0) {
+                            skipBlock = result;
+                        } else {
+                            std::cout << "Else nested" << std::endl;
+                        }
                         this->getNextToken(); // close tag
                     }
 
                     if (strcmp(keyword, "endif") == 0) {
+                        if (levelNested == 0) {
+                            stopRender = true;
+                        } else {
+                            std::cout << "nested endif out" << std::endl;
+                            levelNested--;
+                        }
                         this->getNextToken(); // close tag
-                        stopRender = true;
                     }
                     break;
                 }
 
                 default: {
                     std::cout << "Line: " << token->getLine() << "; Column: " << token->getColumn() << std::endl;
+                    std::cout << "Type: " << token->getType() << std::endl;
+                    INIT_CHAR_STRING(str, 1024)
+                    RESET_TOKEN_READER(token)
+                    token->getReader()->read(str, 1024);
+                    std::cout << "Value: " << str << std::endl;
                     throw new UnexpectedToken;
                 }
             }
