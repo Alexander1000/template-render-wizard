889a9cee02ead9baa633446a6daa096f59ee0a29
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-12-13 11:56:46 +0300

    token parser

--- include/trw/syntax.h
+++ include/trw/syntax.h
@@ -2,16 +2,27 @@
 #define H_TRW_SYNTAX_INCLUDED
 
 #include <io-buffer.h>
+#include <trw/token.h>
+#include <map>
+#include <string>
 
 namespace TemplateRenderWizard
 {
+    class UnknownToken
+    {};
+
     class TokenFile
     {
     public:
         TokenFile(const char* tokenFile);
+        TemplateRenderWizard::Token::Type getNextTokenType();
+        TemplateRenderWizard::Token::Type getTokenTypeByName(const char*);
 
     private:
         IOBuffer::CharStream* charStream;
+        std::map<std::string, TemplateRenderWizard::Token::Type>* tokenMap;
+
+        bool isEmptyChar(char*);
     };
 }
 
--- src/trw/syntax/syntax.cpp
+++ src/trw/syntax/syntax.cpp
@@ -1,4 +1,7 @@
 #include <trw.h>
+#include <io-buffer.h>
+#include <map>
+#include <string>
 
 namespace TemplateRenderWizard
 {
@@ -7,5 +10,51 @@ namespace TemplateRenderWizard
         IOBuffer::IOFileReader* fileReader;
         fileReader = new IOBuffer::IOFileReader(tokenFile);
         this->charStream = new IOBuffer::CharStream(fileReader);
+
+        this->tokenMap = new std::map<std::string, Token::Type>;
+        (*this->tokenMap)["plainText"] = Token::Type::PlainTextType;
+    }
+
+    Token::Type TokenFile::getNextTokenType()
+    {
+        char* nextChar = this->charStream->getNext();
+        if (nextChar == nullptr) {
+            return Token::EofType;
+        }
+
+        IOBuffer::IOMemoryBuffer* ioWriter = nullptr;
+
+        while (this->isEmptyChar(nextChar)) {
+            nextChar = this->charStream->getNext();
+        }
+        if (nextChar == nullptr) {
+            return Token::EofType;
+        }
+
+        ioWriter = new IOBuffer::IOMemoryBuffer(16);
+        int length = 0;
+
+        while (!this->isEmptyChar(nextChar)) {
+            ioWriter->write(nextChar, 1);
+            length++;
+        }
+
+        length++;
+        INIT_CHAR_STRING(strTokenName, length)
+        ioWriter->read(strTokenName, length - 1);
+
+        return this->getTokenTypeByName(strTokenName);
+    }
+
+    bool TokenFile::isEmptyChar(char* symbol) {
+        return symbol == nullptr || *symbol == ' ' || *symbol == 0x0A;
+    }
+
+    Token::Type TokenFile::getTokenTypeByName(const char *tokenName) {
+        if (this->tokenMap->find(tokenName) != this->tokenMap->end()) {
+            return this->tokenMap->at(tokenName);
+        }
+
+        throw new UnknownToken;
     }
 }
