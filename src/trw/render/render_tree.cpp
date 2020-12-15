ca08596c6c0453ba690481cdee6943daa0106c84
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-12-15 08:23:11 +0300

    render

--- src/trw/render/render_tree.cpp
+++ src/trw/render/render_tree.cpp
@@ -4,5 +4,14 @@ namespace TemplateRenderWizard
 {
     void Render::render_tree(IOBuffer::IOBuffer *buffer, Syntax::SyntaxElement *treeElement)
     {
+        if (treeElement->getType() == Syntax::SyntaxElementType::SyntaxType) {
+            this->render_tree(buffer, treeElement->getElement());
+            return;
+        }
+
+        if (treeElement->getType() == Syntax::SyntaxElementType::TokenListType) {
+            // todo: call sub sequence renders
+            return;
+        }
     }
 }
