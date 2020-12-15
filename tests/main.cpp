c2507a6dbd4ad731340de076ee2049eea489fc43
Aleksandr Dankovtsev <a.dankovtsev@mail.ru>
2020-12-15 10:07:48 +0300

    add test render

--- tests/main.cpp
+++ tests/main.cpp
@@ -164,6 +164,45 @@ CppUnitTest::TestCase* testRender_Template_Positive(char* templateName, char* va
     return t;
 }
 
+CppUnitTest::TestCase* testRenderWithSyntaxTree_Template_Positive(char* templateName, char* valuesFile)
+{
+    CppUnitTest::TestCase* t = nullptr;
+    t = new CppUnitTest::TestCase(valuesFile);
+
+    t->printTitle();
+
+    TemplateRenderWizard::Tree::Tree tree;
+    INIT_CHAR_STRING(fileWithValues, 1024)
+    sprintf(fileWithValues, "./fixtures/%s", valuesFile);
+    tree.scan(fileWithValues);
+
+    TemplateRenderWizard::Render* render;
+    INIT_CHAR_STRING(fileWithTemplate, 1024)
+    sprintf(fileWithTemplate, "./fixtures/%s", templateName);
+    render = new TemplateRenderWizard::Render(fileWithTemplate, &tree);
+
+    IOBuffer::IOMemoryBuffer* buffer = render->toBufferTree();
+
+    INIT_CHAR_STRING(tBuffer, 1024);
+    buffer->read(tBuffer, 1024);
+
+    INIT_CHAR_STRING(expected, 1024)
+    INIT_CHAR_STRING(fileExpected, 1024)
+    memcpy(fileExpected, valuesFile, sizeof(char) * (strlen(valuesFile) - 5));
+
+    INIT_CHAR_STRING(strFileExpected, 1024)
+    sprintf(strFileExpected, "./fixtures/%s.out", fileExpected);
+
+    IOBuffer::IOFileReader* fileReader;
+    fileReader = new IOBuffer::IOFileReader(strFileExpected);
+    fileReader->read(expected, 1024);
+
+    CppUnitTest::assertEquals(t, expected, tBuffer);
+
+    t->finish();
+    return t;
+}
+
 CppUnitTest::TestCase* testLexer_Template_Positive(char* templateName)
 {
     CppUnitTest::TestCase* t = nullptr;
@@ -285,6 +324,7 @@ void scanTests(CppUnitTest::TestSuite* testSuite) {
 
             if (strcmp(strTplNum, strYamlNum) == 0) {
                 testSuite->addTestCase(testRender_Template_Positive(namelist[i]->d_name, yamlList[j]->d_name));
+                testSuite->addTestCase(testRenderWithSyntaxTree_Template_Positive(namelist[i]->d_name, yamlList[j]->d_name));
             }
         }
 
