65867c6fa8d7bf9e657786075c5d5ccd0b51eb57
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-01-03 15:30:58 +0300

    declare new tokens

--- include/trw/token.h
+++ include/trw/token.h
@@ -133,6 +133,41 @@ namespace TemplateRenderWizard::Token
         Comma(int line, int column);
         int getType() final;
     };
+
+    class FilePath : public SyntaxTree::Token::Token
+    {
+    public:
+        FilePath(int line, int column, IOBuffer::IOReader *reader);
+        int getType() final;
+    };
+
+    class DoubleDot : public SyntaxTree::Token::Token
+    {
+    public:
+        DoubleDot(int line, int column);
+        int getType() final;
+    };
+
+    class OpenBrace : public SyntaxTree::Token::Token
+    {
+    public:
+        OpenBrace(int line, int column);
+        int getType() final;
+    };
+
+    class CloseBrace : public SyntaxTree::Token::Token
+    {
+    public:
+        CloseBrace(int line, int column);
+        int getType() final;
+    };
+
+    class IncludeWithKey : public SyntaxTree::Token::Token
+    {
+    public:
+        IncludeWithKey(int line, int column, IOBuffer::IOReader *reader);
+        int getType() final;
+    };
 }
 
 #endif
