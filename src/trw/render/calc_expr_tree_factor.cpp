058e1d9a16688f508b71fc38630f6bcfcda89a8a
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-01-04 20:07:48 +0300

    calc new expr

--- src/trw/render/calc_expr_tree_factor.cpp
+++ src/trw/render/calc_expr_tree_factor.cpp
@@ -1,9 +1,26 @@
 #include <trw.h>
+#include <iostream>
 
 namespace TemplateRenderWizard
 {
     Value * Render::calc_expr_tree_factor(SyntaxTree::Syntax::SyntaxElement *syntaxElement, Context *context)
     {
+        if (syntaxElement->getType() == SyntaxTree::Syntax::SyntaxElementType::TokenListType) {
+            auto it = syntaxElement->getListElements()->begin();
+            int size = syntaxElement->getListElements()->size();
+            if (size == 1) {
+                auto firstElement = *it;
+                std::cout << std::endl;
+                if (firstElement->getType() == SyntaxTree::Syntax::SyntaxElementType::TokenType) {
+                    return this->getValueFromToken(firstElement->getToken(), context);
+                }
+                throw new UnexpectedToken;
+            }
+
+            throw new UnexpectedToken;
+        }
+
+        throw new UnexpectedToken;
         return nullptr;
     }
 }
--- src/trw/render/calc_expr_tree_term.cpp
+++ src/trw/render/calc_expr_tree_term.cpp
@@ -1,5 +1,7 @@
 #include <trw.h>
 #include <syntax-tree-lib.h>
+#include <iostream>
+#include <cstring>
 
 namespace TemplateRenderWizard
 {
@@ -8,7 +10,18 @@ namespace TemplateRenderWizard
         if (syntaxElement->getType() == SyntaxTree::Syntax::SyntaxElementType::TokenListType) {
             int size = syntaxElement->getListElements()->size();
             auto it = syntaxElement->getListElements()->begin();
+            auto firstElement = (*it)->getElement();
+            if (size == 1) {
+                if (strcmp(firstElement->getRule()->getName(), "factor") == 0) {
+                    return this->calc_expr_tree_factor(firstElement, context);
+                }
+            }
+
+            throw new UnexpectedToken;
         }
+
+        throw new UnexpectedToken;
+
         return nullptr;
     }
 }
