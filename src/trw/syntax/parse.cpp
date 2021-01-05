bb6a20300a28c9a6495534d9681f257a6a87e5c2
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-01-05 11:09:25 +0300

    parse brackets

--- src/trw/syntax/parse.cpp
+++ src/trw/syntax/parse.cpp
@@ -1,10 +1,41 @@
 #include <trw.h>
 #include <syntax-tree-lib.h>
+#include <list>
 
 namespace TemplateRenderWizard::Syntax
 {
     SyntaxTree::Syntax::SyntaxElement* Tree::beforeParse(std::list<SyntaxTree::Syntax::SyntaxElement *> *elements)
     {
-        return this->parse(elements);
+        auto filteredElements = new std::list<SyntaxTree::Syntax::SyntaxElement*>;
+
+        for (auto it = elements->begin(); it != elements->end(); it++) {
+            auto token = (*it)->getToken();
+            auto bracketElements = new std::list<SyntaxTree::Syntax::SyntaxElement*>;
+            int nestedLevel = 0;
+
+            if (token->getType() == TemplateRenderWizard::Token::Type::RoundBracketOpenType) {
+                do {
+                    it++;
+                    if (std::next(it) == elements->end()) {
+                        break;
+                    }
+                    if ((*it)->getToken()->getType() == TemplateRenderWizard::Token::Type::RoundBracketOpenType) {
+                        nestedLevel++;
+                    }
+                    if ((*it)->getToken()->getType() == TemplateRenderWizard::Token::Type::RoundBracketCloseType) {
+                        if (nestedLevel > 0) {
+                            nestedLevel--;
+                        } else {
+                            break;
+                        }
+                    }
+                    bracketElements->push_back(*it);
+                } while (true);
+            }
+
+            filteredElements->push_back(*it);
+        }
+
+        return this->parse(filteredElements);
     }
 }
