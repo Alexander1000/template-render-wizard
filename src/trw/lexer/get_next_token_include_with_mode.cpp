13c6be61aae7174a0cb0110dcdb894d44245eb6e
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-01-17 11:49:55 +0300

    parse braces

--- src/trw/lexer/get_next_token_include_with_mode.cpp
+++ src/trw/lexer/get_next_token_include_with_mode.cpp
@@ -5,6 +5,31 @@ namespace TemplateRenderWizard::Lexer
 {
     SyntaxTree::Token::Token* Lexer::getNextTokenIncludeWithMode()
     {
+        char* curSymbol = this->getNextChar();
+        if (curSymbol == nullptr) {
+            return nullptr;
+        }
+
+        if (*curSymbol == 0x20) {
+            // skip spaces
+            do {
+                curSymbol = this->getNextChar();
+            } while(curSymbol != nullptr && *curSymbol == 0x20);
+        }
+
+        if (curSymbol == nullptr) {
+            return nullptr;
+        }
+
+        if (*curSymbol == '{') {
+            return new Token::OpenBrace(this->position->getLine(), this->position->getColumn());
+        }
+
+        if (*curSymbol == '}') {
+            this->switchToPreviousMode();
+            return new Token::CloseBrace(this->position->getLine(), this->position->getColumn());
+        }
+
         return nullptr;
     }
 }
