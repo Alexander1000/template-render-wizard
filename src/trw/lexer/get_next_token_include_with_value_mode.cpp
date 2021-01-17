7aad6f9aa084e5bdbd57aa1685bafa071f5e8d18
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-01-17 12:43:45 +0300

    skip spaces

--- src/trw/lexer/get_next_token_include_with_value_mode.cpp
+++ src/trw/lexer/get_next_token_include_with_value_mode.cpp
@@ -5,6 +5,22 @@ namespace TemplateRenderWizard::Lexer
 {
     SyntaxTree::Token::Token* Lexer::getNextTokenIncludeWithValueMode()
     {
+        char* curSymbol = this->getNextChar();
+        if (curSymbol == nullptr) {
+            return nullptr;
+        }
+
+        if (*curSymbol == 0x20) {
+            // skip spaces
+            do {
+                curSymbol = this->getNextChar();
+            } while(curSymbol != nullptr && *curSymbol == 0x20);
+        }
+
+        if (curSymbol == nullptr) {
+            return nullptr;
+        }
+        
         return nullptr;
     }
 }
