5ece78fc7eaf8f5bc7f47de18f81424dda5e41a1
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-01-17 13:15:25 +0300

    parse lexer

--- src/trw/lexer/get_next_token_include_with_value_mode.cpp
+++ src/trw/lexer/get_next_token_include_with_value_mode.cpp
@@ -21,6 +21,22 @@ namespace TemplateRenderWizard::Lexer
             return nullptr;
         }
 
-        return nullptr;
+        if (*curSymbol == ',') {
+            this->switchToPreviousMode();
+            return new Token::Comma(this->position->getLine(), this->position->getColumn());
+        }
+
+        auto ioWriter = new IOBuffer::IOMemoryBuffer(64);
+        while (curSymbol != nullptr) {
+            if (*curSymbol != ',') {
+                ioWriter->write(curSymbol, 1);
+            } else {
+                this->pushStackChar(curSymbol);
+                break;
+            }
+            curSymbol = this->getNextChar();
+        }
+
+        return new Token::ExpressionValue(this->position->getLine(), this->position->getColumn(), ioWriter);
     }
 }
